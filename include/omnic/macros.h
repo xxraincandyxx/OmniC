e7cbf37a2cfc6fa9ee87505fe72f8bdb5172bcf1
ra1n <xxraincandyxx@gmail.com>
2025-10-03 15:50:57 +0800

    docs(clang:macros): add docs; header guard

--- include/omnic/macros.h
+++ include/omnic/macros.h
@@ -1,3 +1,17 @@
+// -*- mode: C; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2; -*-
+
+#ifndef OMNIC_MACROS_H_
+#define OMNIC_MACROS_H_
+
+/* -------------------------------------------------------------------------- */
+
+/**
+ * @file macros.h
+ * @brief Header for macro utilities.
+ */
+
+/* -------------------------------------------------------------------------- */
+
 // Assertion macro for general conditions
 #define ASSERT(condition, message)                                   \
   do {                                                               \
@@ -19,3 +33,5 @@
       g_test_failures++;                                                    \
     }                                                                       \
   } while ((void)0, 0)
+
+#endif  // OMNIC_MACROS_H_
