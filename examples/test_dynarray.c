64a4bc498bd343e7edab4fc08a8761b9027a5cad
ra1n <xxraincandyxx@gmail.com>
2025-10-03 15:35:32 +0800

    feat(clang): init. macros

--- examples/test_dynarray.c
+++ examples/test_dynarray.c
@@ -4,34 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <omnic/macros.h>
 #include <omnic/dynarray.h>
 
 // --- Test Framework Setup ---
 
 // Global counter for failed tests
 static int g_test_failures = 0;
-
-// Assertion macro for general conditions
-#define ASSERT(condition, message)                                   \
-  do {                                                               \
-    if (!(condition)) {                                              \
-      (void)fprintf(stderr, "FAIL: %s:%d: %s\n", __FILE__, __LINE__, \
-                    (message));                                      \
-      g_test_failures++;                                             \
-    }                                                                \
-  } while ((void)0, 0)
-
-// Assertion macro for comparing values (provides more context on failure)
-#define ASSERT_EQ(a, b, fmt, message)                                       \
-  do {                                                                      \
-    if ((a) != (b)) {                                                       \
-      (void)fprintf(stderr, "FAIL: %s:%d: %s\n", __FILE__, __LINE__,        \
-                    (message));                                             \
-      (void)fprintf(stderr, "      Expected: " fmt ", Got: " fmt "\n", (b), \
-                    (a));                                                   \
-      g_test_failures++;                                                    \
-    }                                                                       \
-  } while ((void)0, 0)
 
 // --- Test Functions ---
 
